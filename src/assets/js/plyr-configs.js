edb29871e5c4fe9a1858def0aa80b34a2fd84d79
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-02-11 04:58:01 -0400

    Autocommit por GitHub: alterações: add 'src/assets/js/plyr-configs.js'

--- src/assets/js/plyr-configs.js
+++ src/assets/js/plyr-configs.js
@@ -4,19 +4,11 @@ const plyrControls = `
         <svg role="presentation"><use xlink:href="#plyr-restart"></use></svg>
         <span class="plyr__tooltip" role="tooltip">Restart</span>
     </button>
-    <button type="button" class="plyr__control" data-plyr="rewind">
-        <svg role="presentation"><use xlink:href="#plyr-rewind"></use></svg>
-        <span class="plyr__tooltip" role="tooltip">Rewind {seektime} secs</span>
-    </button>
     <button type="button" class="plyr__control" aria-label="Play, {title}" data-plyr="play">
         <svg class="icon--pressed" role="presentation"><use xlink:href="#plyr-pause"></use></svg>
         <svg class="icon--not-pressed" role="presentation"><use xlink:href="#plyr-play"></use></svg>
         <span class="label--pressed plyr__tooltip" role="tooltip">Pause</span>
         <span class="label--not-pressed plyr__tooltip" role="tooltip">Play</span>
-    </button>
-    <button type="button" class="plyr__control" data-plyr="fast-forward">
-        <svg role="presentation"><use xlink:href="#plyr-fast-forward"></use></svg>
-        <span class="plyr__tooltip" role="tooltip">Forward {seektime} secs</span>
     </button>
     <div class="plyr__progress">
         <input data-plyr="seek" type="range" min="0" max="100" step="0.01" value="0" aria-label="Seek">
